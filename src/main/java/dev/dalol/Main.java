899036f74d892635c50b77da964c69deea6216e6
xdalul <dalol.dev@gmail.com>
2023-09-16 11:48:18 +0200

    Token Test

--- src/main/java/dev/dalol/Main.java
+++ src/main/java/dev/dalol/Main.java
@@ -16,10 +16,6 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class Main {
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.configure()
-                .directory("src/main/resources")
-                .filename("env")
-                .load();
 
 
         JDA jda = JDABuilder.createDefault(dotenv.get("TOKEN"))
