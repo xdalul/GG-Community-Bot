32afc6eddbc48a739648edf7c8ebcf3001e7cc58
xdalul <101058306+xdalul@users.noreply.github.com>
2024-01-10 14:12:50 +0100

    Revert "a"

--- src/main/java/dev/dalol/Main.java
+++ src/main/java/dev/dalol/Main.java
@@ -87,7 +87,6 @@ public class Main {
 
         jda.upsertCommand("enable-oneword", "Schaltet OneWord aus/an")
                 .addOption(OptionType.BOOLEAN, "boolean", "true/false", true).queue();
-
         jda.upsertCommand("bot-maintenance", "Schaltet die Bot-Wartungsarbeiten an/aus")
                 .addOption(OptionType.BOOLEAN, "boolean", "true/false", true).queue();
     }
